b36a963a4dec6f9eb013895e2e05beaef26dc413
Sherry <huizezhangsh@gmail.com>
2021-03-18 21:58:32 +1100

    a plot for various tour display

--- R/tour-notation.R
+++ R/tour-notation.R
@@ -5,3 +5,26 @@ p <- PPtreeViz::Huberplot(iris[,1:2],iris[,5],PPmethod="LDA")
 ggsave(p, filename = "notation-huber.pdf", 
        path = here::here("figures"),
        width = 12, height = 5, units = "in")
+
+# figure 3 diagram illustrating different displays for different d
+library(tourr)
+render(flea[,1:6], 
+       guided_tour(holes(), d= 1),
+       display_dist(),
+       "pdf", 
+       frames = 1,
+       file.path(here::here("figures", "notation-display-1d.pdf")))
+
+render(flea[,1:6], 
+       guided_tour(holes(), d= 2),
+       display_xy(),
+       "pdf", 
+       frames = 1,
+       file.path(here::here("figures", "notation-display-2d.pdf")))
+
+render(flea[,1:6], 
+       guided_tour(holes(), d= 5),
+       display_pcp(),
+       "pdf", 
+       frames = 1,
+       file.path(here::here("figures", "notation-display-5d.pdf")))
